7c90058610b17b353c0b6863f2c96f96d2b97560
Dave <daveg7lee@gmail.com>
2021-04-25 13:00:46 +0900

    AppLoading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromise = fontsToLoad.map((font: any) => Font.loadAsync(font));
+    console.log(fontPromise);
+    await Promise.all(fontPromise);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
