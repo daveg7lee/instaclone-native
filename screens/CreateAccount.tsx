67854e767e60384f80317e81bba3aa1148847aea
Dave <daveg7lee@gmail.com>
2021-04-26 18:13:26 +0900

    Natural Forms part One

--- screens/CreateAccount.tsx
+++ screens/CreateAccount.tsx
@@ -1,42 +1,62 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { TextInput } from 'react-native-gesture-handler';
 import AuthButton from '../components/auth/AuthButton';
 import AuthLayout from '../components/auth/AuthLayout';
 
 const CreateAccount = () => {
+  const lastNameRef = useRef(null);
+  const userNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const onNext = (nextOne) => {
+    nextOne?.current?.focus();
+  };
+  const onDone = () => {
+    alert('done!');
+  };
   return (
     <AuthLayout>
       <TextInput
+        autoFocus
         placeholder="First Name"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: 'white', width: '100%' }}
+        onSubmitEditing={() => onNext(lastNameRef)}
       />
       <TextInput
+        ref={lastNameRef}
         placeholder="Last Name"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: 'white', width: '100%' }}
+        onSubmitEditing={() => onNext(userNameRef)}
       />
       <TextInput
+        ref={userNameRef}
         placeholder="Username"
         placeholderTextColor="gray"
         returnKeyType="next"
         style={{ backgroundColor: 'white', width: '100%' }}
+        onSubmitEditing={() => onNext(emailRef)}
       />
       <TextInput
+        ref={emailRef}
         placeholder="Email"
         placeholderTextColor="gray"
         keyboardType="email-address"
         returnKeyType="next"
         style={{ backgroundColor: 'white', width: '100%' }}
+        onSubmitEditing={() => onNext(passwordRef)}
       />
       <TextInput
+        ref={passwordRef}
         placeholder="Password"
         placeholderTextColor="gray"
         secureTextEntry
         returnKeyType="done"
         style={{ backgroundColor: 'white', width: '100%' }}
+        onSubmitEditing={onDone}
       />
       <AuthButton text="Create Account" disabled={true} onPress={() => {}} />
     </AuthLayout>
