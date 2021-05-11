af7b8c7a9af33c08fdfe14f60d035a22c0137185
Dave <daveg7lee@gmail.com>
2021-05-11 20:39:27 +0900

    Room Screen part Two

--- screens/Room.tsx
+++ screens/Room.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import React, { useEffect } from 'react';
-import { Text } from 'react-native';
+import { KeyboardAvoidingView } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
 import ScreenLayout from '../components/ScreenLayout';
 
 const ROOM_QUERY = gql`
@@ -20,22 +22,66 @@ const ROOM_QUERY = gql`
   }
 `;
 
+const MessageContainer = styled.View``;
+const Author = styled.View``;
+const Avatar = styled.Image``;
+const Username = styled.Text`
+  color: white;
+`;
+const Message = styled.Text`
+  color: white;
+`;
+const TextInput = styled.TextInput`
+  margin-bottom: 50px;
+  width: 95%;
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 1000px;
+`;
+
 export default function Room({
   route: {
     params: { id, talkingTo },
   },
   navigation,
 }) {
-  const { data } = useQuery(ROOM_QUERY, { variables: { id } });
-  console.log(data);
+  const { data, loading, refetch } = useQuery(ROOM_QUERY, {
+    variables: { id },
+  });
   useEffect(() => {
     navigation.setOptions({
       title: talkingTo?.username,
     });
   }, []);
+  const renderItem = ({ item: message }) => (
+    <MessageContainer>
+      <Author>
+        <Avatar source={{ uri: message.user.avatar }} />
+        <Username>{message.user.username}</Username>
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
   return (
-    <ScreenLayout>
-      <Text>Room</Text>
-    </ScreenLayout>
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: 'black' }}
+      behavior="height"
+      keyboardVerticalOffset={100}
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          inverted
+          style={{ width: '100%' }}
+          data={data?.seeRoom?.messages}
+          keyExtractor={(message) => '' + message.id}
+          renderItem={renderItem}
+        />
+        <TextInput
+          placeholder="Write a message..."
+          returnKeyLabel="Send Message"
+          returnKeyType="send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 }
